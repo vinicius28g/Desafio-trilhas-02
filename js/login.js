85e57da77423428aac2423896da670a00b23e2d8
Vinicius Guimarães <guimaraes14vinicius@gmail.com>
2025-04-06 22:36:59 -0300

    criação de tela de login

---
+++ js/login.js
@@ -0,0 +1,15 @@
+
+function toggleSenha() {
+    const input = document.getElementById("senha");
+    const icon = document.querySelector(".toggle-olho");
+
+    if (input.type === "password") {
+      input.type = "text";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    } else {
+      input.type = "password";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+    }
+  }
